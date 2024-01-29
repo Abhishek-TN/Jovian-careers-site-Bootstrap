ed1e5bd8059816efb6010eae77faad8a613b5834
Abhishek T N <tnabhishek03@gmail.com>
2024-01-29 13:14:59 +0000

    implemented express

--- src/app.js
+++ src/app.js
@@ -4,3 +4,10 @@ const app = express();
 app.get('/',(req,res)=>{
     res.send("Hello world!");
 });
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, ()=>{
+    console.log('Server running on https://localhost:${port}');
+});
+
