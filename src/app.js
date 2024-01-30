ac8c25da62ed8ff4b82534fee65dd11a0fe44398
Abhishek T N <tnabhishek03@gmail.com>
2024-01-30 08:22:03 +0000

    added express app and ststic files

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+
+app.use(express.static(path.join(__dirname,'public')));
+
 app.get('/',(req,res)=>{
     //res.send("Hello world!");
     res.sendFile(path.join(__dirname,'index.html'));
